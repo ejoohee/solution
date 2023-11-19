21fb37c2b21a18c9bdb10da1265855a53e542c6d
Lee JooHee <122416957+ejoohee@users.noreply.github.com>
2023-11-20 08:25:55 +0900

    [Silver I] Title: 포도주 시식, Time: 376 ms, Memory: 29132 KB -BaekjoonHub

---
+++ 백준/Silver/2156. 포도주 시식/포도주 시식.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt(); 
+        int[] wine = new int[n + 1];
+        for (int i = 1; i <= n; i++) {
+            wine[i] = sc.nextInt(); 
+        }
+
+        long[] dp = new long[n + 1];
+        
+        dp[1] = wine[1];
+        if (n > 1) {
+            dp[2] = wine[1] + wine[2];
+        }
+
+         for (int i = 3; i <= n; i++) {
+            dp[i] = Math.max(dp[i - 1], Math.max(dp[i - 2] + wine[i], dp[i - 3] + wine[i - 1] + wine[i]));
+        }
+
+        System.out.println(dp[n]); 
+        sc.close(); 
+    }
+}
