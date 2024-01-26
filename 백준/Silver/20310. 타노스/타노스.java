1684a19b9803eacec5e6311553da18ac85af107d
Lee JooHee <122416957+ejoohee@users.noreply.github.com>
2024-01-26 15:03:41 +0900

    [Silver III] Title: 타노스, Time: 208 ms, Memory: 17668 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/20310. 타노스/타노스.java
+++ 백준/Silver/20310. 타노스/타노스.java
@@ -1,24 +1,37 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String str = sc.next();
-        StringBuilder sb = new StringBuilder();
-        int cnt0=0;
-        int cnt1=0;
-        for (int i=0; i<str.length(); i++){
-            if(str.charAt(i)=='0') {
-                cnt0++;
-            }else cnt1++;
+        List<Character> list = new ArrayList<>();
+
+        for (char c : str.toCharArray()){
+            list.add(c);
         }
 
-        for (int i=0; i<cnt0/2; i++){
-            sb.append("0");
+        int cnt0 = Collections.frequency(list,'0')/2;
+        int cnt1 = Collections.frequency(list,'1')/2;
+
+        for (int i=0; i<cnt1; i++){
+            list.remove(list.indexOf('1'));
         }
-        for (int i=0; i<cnt1/2; i++){
-            sb.append("1");
+
+        for (int i = list.size() - 1; i >= 0 && cnt0 > 0; i--) {
+            if (list.get(i) == '0') {
+                list.remove(i);
+                cnt0--;
+            }
         }
+
+        StringBuilder sb = new StringBuilder();
+        for (char c : list){
+            sb.append(c);
+        }
+
         System.out.println(sb);
     }
 }
